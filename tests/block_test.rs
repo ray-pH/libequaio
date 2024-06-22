c2bc63973367ec35ddf311977b2cf23e389d8b9b
ray-pH <rayhan.azizi9@gmail.com>
2024-06-22 20:50:23 +0700

    block: add extra test

--- tests/block_test.rs
+++ tests/block_test.rs
@@ -3,7 +3,9 @@ use equaio::expression::Address;
 use equaio::vec_strings;
 use equaio::parser_prefix;
 use equaio::address;
+use equaio::arithmetic::get_arithmetic_ctx;
 use equaio::block::{Block, block_builder};
+use equaio::worksheet::Worksheet;
 
 #[cfg(test)]
 mod simple_block {
@@ -125,6 +127,54 @@ mod simple_block {
                 bb::symbol("+".to_string(), address![1].sub(1)),
                 bb::symbol("d".to_string(), address![1,2]),
             ], address![1]),
+        ], address![]);
+        assert_eq!(block, expected_block);
+    }
+    
+    #[test]
+    fn algebra_with_number(){
+        let ctx = get_arithmetic_ctx().add_param("x".to_string());
+        let expr = parser_prefix::to_expression("=(-(*(2,x),1),3)", &ctx).unwrap();
+        assert_eq!(expr.to_string(true), "(((2 * x) - 1) = 3)");
+        let block = Block::from(expr);
+        let expected_block = bb::horizontal_container(vec![
+            bb::horizontal_container(vec![
+                bb::horizontal_container(vec![
+                    bb::symbol("2".to_string(), address![0,0,0]),
+                    bb::symbol("*".to_string(), address![0,0]),
+                    bb::symbol("x".to_string(), address![0,0,1]),
+                ], address![0,0]),
+                bb::symbol("-".to_string(), address![0]),
+                bb::symbol("1".to_string(), address![0,1]),
+            ], address![0]),
+            bb::symbol("=".to_string(), address![]),
+            bb::symbol("3".to_string(), address![1]),
+        ], address![]);
+        assert_eq!(block, expected_block);
+    }
+    
+    #[test]
+    fn form_worksheet(){
+        let mut ws = Worksheet::init_algebra_worksheet(vec_strings!["x"]);
+        let expr = parser_prefix::to_expression("=(-(*(2,x),1),3)", &ws.get_expression_context()).unwrap();
+        assert_eq!(expr.clone().to_string(true), "(((2 * x) - 1) = 3)");
+        ws.introduce_expression(expr.clone());
+        let seq0 = ws.get_expression_sequence(0).unwrap();
+        let expr0 = seq0.last_expression();
+        assert_eq!(expr0.to_string(true), "(((2 * x) - 1) = 3)");
+        let block = Block::from(expr0.clone());
+        let expected_block = bb::horizontal_container(vec![
+            bb::horizontal_container(vec![
+                bb::horizontal_container(vec![
+                    bb::symbol("2".to_string(), address![0,0,0]),
+                    bb::symbol("*".to_string(), address![0,0]),
+                    bb::symbol("x".to_string(), address![0,0,1]),
+                ], address![0,0]),
+                bb::symbol("-".to_string(), address![0]),
+                bb::symbol("1".to_string(), address![0,1]),
+            ], address![0]),
+            bb::symbol("=".to_string(), address![]),
+            bb::symbol("3".to_string(), address![1]),
         ], address![]);
         assert_eq!(block, expected_block);
     }
